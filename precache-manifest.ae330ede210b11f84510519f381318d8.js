cba3d3466e1cb86fcfd74db45e541560c2c85a4a
Shubham Agarwal <shubhamagarwal.alld@gmail.com>
2021-08-28 11:30:17 +0530

    Updates

---
+++ precache-manifest.ae330ede210b11f84510519f381318d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025d3530acd3b0a6a7ed76cc675b6844",
+    "url": "/tours/index.html"
+  },
+  {
+    "revision": "7c4a39aba6e2b333a700",
+    "url": "/tours/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/tours/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tours/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c4a39aba6e2b333a700",
+    "url": "/tours/static/js/main.0095c684.chunk.js"
+  },
+  {
+    "revision": "cabdc42b426759795a4b",
+    "url": "/tours/static/js/runtime-main.3b7494d8.js"
+  }
+]);
